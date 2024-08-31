43428132c9ddaa9c80523b39f2e2e5591e8003ec
Oleksandr Dymov <o.dymov@andersenlab.com>
2024-09-01 01:37:57 +0300

    added subscription

--- Bass Booster/App/AppDelegate.swift
+++ Bass Booster/App/AppDelegate.swift
@@ -6,13 +6,14 @@
 //
 
 import UIKit
+import ApphudSDK
 
-final class AppDelegate: NSObject, UIApplicationDelegate {
-    
-    func application(
-        _ application: UIApplication,
-        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
-    ) -> Bool {
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Additional setup if needed
         return true
     }
 }
--- Bass Booster/App/BassBoosterApp.swift
+++ Bass Booster/App/BassBoosterApp.swift
@@ -6,9 +6,16 @@
 //
 
 import SwiftUI
+import ApphudSDK
 
 @main
-struct BassBoosterApp: App {
+struct YourApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
+    init() {
+        Apphud.start(apiKey: "app_2ojGaR5zyQUUJn1htob9ei2uFhZFxv")
+    }
+
     var body: some Scene {
         WindowGroup {
             OnboardingView(state: .initial)
