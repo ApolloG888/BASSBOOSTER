edae66e9b9692a35c6553d22be9f6e0851221a56
Dima Protsak <dmitro.protsak@andersenlab.poqcommerce.com>
2024-08-31 13:56:16 +0200

    Added project

---
+++ Bass Booster/Bass Booster/Bass_BoosterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bass_BoosterApp.swift
+//  Bass Booster
+//
+//  Created by Дмитрий Процак on 31.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct Bass_BoosterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bass Booster/Bass Booster/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bass Booster
+//
+//  Created by Дмитрий Процак on 31.08.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
