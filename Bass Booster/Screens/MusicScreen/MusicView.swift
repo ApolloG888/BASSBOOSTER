f96aaa04407604b7283548f99025f3002546d7cf
Dima Protsak <>
2024-10-14 00:17:14 +0200

    refactor uiview

--- Bass Booster/Screens/MusicScreen/MusicView.swift
+++ Bass Booster/Screens/MusicScreen/MusicView.swift
@@ -195,19 +195,6 @@ struct MusicView: View {
     }
 }
 
-extension View {
-    var deviceCornerRadius: CGFloat {
-        let key = "_displayCornerRadius"
-        if let screen = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.windows.first?.screen {
-            if let cornerRadius = screen.value(forKey: key) as? CGFloat {
-                return cornerRadius
-            }
-            return 0
-        }
-        return 0
-    }
-}
-
 struct MusicView_Previews: PreviewProvider {
     @Namespace static var animation
     static var previews: some View {
